9bd4436e7f297559fe854a53eba295ca3581064f
Itihell Mejia <itihell.mejia@gmail.com>
2021-11-16 20:18:15 -0600

    Validar todos los campos del usuario

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -12,7 +12,18 @@ const router = Router();
 
 router.get("/", index);
 router.put("/:id", edit);
-router.post("/", [check("correo", "El correo no es valido").isEmail()], store);
+router.post(
+  "/",
+  [
+    check("nombre", "El nombre es obligatorio").not().isEmpty(),
+    check("password", "El password debe contener mas 6 caracteres").isLength({
+      min: 6,
+    }),
+    check("correo", "El correo no es valido").isEmail(),
+    check("rol", "No es un rol valido").isIn(["ADMIN_ROLE", "USER_ROLE"]),
+  ],
+  store
+);
 router.delete("/", destroy);
 router.patch("/", update);
 
